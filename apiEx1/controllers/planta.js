73fc4a3ef80660a9a31d6527f9ae1293881d5690
MercurioV <uo282436@uniovi.es>
2023-05-30 16:25:28 +0100

    Delete from api Fixed

--- apiEx1/controllers/planta.js
+++ apiEx1/controllers/planta.js
@@ -104,7 +104,7 @@ module.exports.addPlanta = l => {
 }
 
 module.exports.deletePlanta = id => {
-    return streets.deleteOne({id:id})
+    return streets.deleteOne({Id:id})
             .then(resposta => {
                 return resposta
             })
